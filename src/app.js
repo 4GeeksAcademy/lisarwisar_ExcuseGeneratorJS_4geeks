8ab9696475060aa30c946f601dc2e9367f114113
LisarWisar <60940343+LisarWisar@users.noreply.github.com>
2023-11-16 22:21:43 +0000

    Completed the excuse generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function random_word(word_array){
+  return word_array[getRandomInt(word_array.length-1)];
+}
+
+let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+git a
+console.log(random_word(who)+" "+random_word(action)+" "+random_word(what)+" "+random_word(when));
